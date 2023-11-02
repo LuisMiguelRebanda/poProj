5805da41ccd3c02fc4877e61c58a944877b00b22
LuisMiguelRebanda <miguel.rebanda@tecnico.ulisboa.pt>
2023-11-02 05:04:24 +0000

    trim comments

--- xxl/core/Max.java
+++ xxl/core/Max.java
@@ -7,6 +7,7 @@ import java.io.Serial;
 import java.io.Serializable;
 import java.util.List;
 
+/** This function was added in the evaluation */
 public class Max extends IntervalFunction implements Serializable {
     @Serial
     private static final long serialVersionUID = 202308312359L;
